133dcb36db153b5e60932de8e846588b356e631f
Rahim Badsa <developerbadsa@gmail.com>
2023-12-21 22:58:04 +0600

    react hook for added to create task page

--- src/Pages/Dashboard/CreateTask.jsx
+++ src/Pages/Dashboard/CreateTask.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const CreateTask = () => {
+      const { register, handleSubmit } = useForm()
+
+      const handleTaskSubmit = (data) => {
+            console.log('click', data)
+      }
+      console.log(register)
       return (
             <div>
-                  create task
+                  <form className='grid px-6 py-10 gap-6' onSubmit={handleSubmit(handleTaskSubmit)}>
+
+                        <label className='flex gap-5 items-center justify-between'>
+                             <span className='font-bold'> Title</span>: <input className="input input-sm input-bordered lg:w-[800px] " placeholder='Type Title Of Your Task' type="text" {...register('title')} />
+                        </label>
+                        <label className='flex items-center gap-6 justify-between'>
+                               <span className='font-bold'>Descriptions</span> : <textarea className="lg:w-[800px] textarea textarea-bordered" {...register('description')}></textarea>
+                        </label>
+                        <div className='grid grid-cols-2'>
+                              <label>
+                                    <span className='font-bold'> Deadlines(Select end Date) </span>: <input className='input' type="date" {...register('date')} />
+                              </label>
+                              <label className='flex items-center gap-6'>
+                                    <span className='font-bold'> Priority </span>:
+                                     <select defaultValue='Select Priority' className='select select-bordered w-full select-sm' {...register('name')}>
+                                          <option value="Select Priority" disabled>Select Priority</option>
+                                          <option value="Low">Low</option>
+                                          <option value="Moderate">Moderate</option>
+                                          <option value="High">High</option>
+                                    </select>
+                              </label>
+                        </div>
+
+
+                        <button className="btn text-white btn-success" type='submit'>Submit</button>
+                  </form>
             </div>
       );
 };
--- src/PrivateRoutes/LoggedIn.jsx
+++ src/PrivateRoutes/LoggedIn.jsx
@@ -2,26 +2,26 @@ import React, { useEffect } from 'react';
 import useUserInfo from '../Hooks/useUserInfo/useUserInfo';
 import { useNavigate } from 'react-router-dom';
 
-const LoggedIn = ({children}) => {
+const LoggedIn = ({ children }) => {
 
-      const {user, loading} = useUserInfo()
-      const navigate = useNavigate()
-
-console.log(user)
+  const { user, loading } = useUserInfo()
+  const navigate = useNavigate()
 
 
-useEffect(() => {
-      if (!user) {
-        navigate('/login');
-      }
-    }, [user, navigate]);
-    
-    if(loading){
-      return 'loading'
-}
+  useEffect(() => {
+    if (loading) {
+       'loading'
+    }else if (!user) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
 
-    return children
-     
+  if (loading) {
+    return 'loading'
+  }
+
+  return children
+
 };
 
 export default LoggedIn;
