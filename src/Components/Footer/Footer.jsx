f525d3fb567662beaa782425ce98dcfc272cb89e
Rahim Badsa <developerbadsa@gmail.com>
2023-12-22 11:12:07 +0600

    create task responsive done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
       return (
-            <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded ">
+            <footer className="footer footer-center p-10 text-base-content rounded h-[43vh] md:h-full">
                   <nav className="grid grid-flow-col gap-4">
                         <a className="link link-hover">About us</a>
                         <a className="link link-hover">Contact</a>
--- src/Pages/Dashboard/CreateTask.jsx
+++ src/Pages/Dashboard/CreateTask.jsx
@@ -6,7 +6,7 @@ import useUserInfo from '../../Hooks/useUserInfo/useUserInfo';
 
 
 const CreateTask = () => {
-      const {user} = useUserInfo()
+      const { user } = useUserInfo()
       const { register, handleSubmit, reset } = useForm()
 
       const handleTaskSubmit = (data) => {
@@ -16,29 +16,29 @@ const CreateTask = () => {
 
             }
 
-            axios.post('http://localhost:3000/create-task',TaskData)
-            .then(res=>{
-                  toast.success("Created Task")
-                  reset()
-            })
+            axios.post('http://localhost:3000/create-task', TaskData)
+                  .then(res => {
+                        toast.success("Created Task")
+                        reset()
+                  })
       }
       return (
-            <div>
+            <div className='my-4'>
                   <form className='grid px-6 py-10 gap-6' onSubmit={handleSubmit(handleTaskSubmit)}>
-                  <ToastContainer />
+                        <ToastContainer />
                         <label className='flex gap-5 items-center justify-between'>
-                             <span className='font-bold'> Title</span>: <input className="input input-sm input-bordered lg:w-[800px] " placeholder='Type Title Of Your Task' type="text" {...register('title')} />
+                              <span className='font-bold'> Title</span>: <input className="input input-sm input-bordered w-full lg:w-[800px] " placeholder='Type Title Of Your Task' type="text" {...register('title')} />
                         </label>
                         <label className='flex items-center gap-6 justify-between'>
-                               <span className='font-bold'>Descriptions</span> : <textarea className="lg:w-[800px] textarea textarea-bordered" {...register('description')}></textarea>
+                              <span className='font-bold'>Descriptions</span> : <textarea className="w-full lg:w-[800px] textarea textarea-bordered" {...register('description')}></textarea>
                         </label>
-                        <div className='grid grid-cols-2'>
-                              <label>
-                                    <span className='font-bold'> Deadlines(Select end Date) </span>: <input className='input' type="date" {...register('date')} />
+                        <div className='grid grid-cols-1 md:grid-cols-2 justify-between'>
+                              <label className='flex items-center'>
+                                    <span className='font-bold text-sm flex flex-nowrap'> Deadlines <span className='hidden md:flex'>(Select end Date)</span> </span>: <input className='input' type="date" {...register('date')} />
                               </label>
                               <label className='flex items-center gap-6'>
                                     <span className='font-bold'> Priority </span>:
-                                     <select defaultValue='Select Priority' className='select select-bordered w-full select-sm' {...register('name')}>
+                                    <select defaultValue='Select Priority' className='select select-bordered w-full select-sm' {...register('name')}>
                                           <option value="Select Priority" disabled>Select Priority</option>
                                           <option value="Low">Low</option>
                                           <option value="Moderate">Moderate</option>
